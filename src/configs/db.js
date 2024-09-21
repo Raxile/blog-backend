f95fa6caab72a8c7bc5327a070c131106147ae27
Piyush Saxena <piyush.saxena@globalvoxinc.com>
2024-09-21 18:47:03 +0530

    On branch feature/add-base-route - remove un nessary console

--- src/configs/db.js
+++ src/configs/db.js
@@ -7,7 +7,6 @@ const DB_URL = process.env.DB_URL.replace(
 ).replace('<db_password>', process.env.DB_PASSWORD);
 
 const db = () => {
-  console.log(DB_URL);
   mongoose.connect(DB_URL);
   mongoose.connection.on('connected', () => {
     console.log('Database connected');
